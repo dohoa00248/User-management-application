fc4db5024ed5271d32df626d47803ada5ceb7a0a
dohoa00248 <dohoa00248@gmail.com>
2025-06-16 12:29:57 +0700

    feat(session): integrate MongoStore for production sessions

--- src/config/db.connect.js
+++ src/config/db.connect.js
@@ -12,10 +12,10 @@
 // // console.log("Check dotenvPath", dotenvPath);
 // configDotenv({ path: dotenvPath });
 
-import { configDotenv } from "dotenv";
-configDotenv();
+import dotenv from 'dotenv';
+dotenv.config();
 // console.log(process.env);
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 const connectToMongoDB = async () => {
   try {
     // const mongodbUrl = "mongodb://localhost:27017";
@@ -24,55 +24,55 @@ const connectToMongoDB = async () => {
     await mongoClient.connect();
     const dbName = process.env.DB_MONGODB_NAME;
     const db = mongoClient.db(dbName);
-    console.log("Successfully connected to MongoDB.");
+    console.log('Successfully connected to MongoDB.');
     return db;
   } catch (error) {
-    console.log("Failed to connect to MongoDB !.", error.message);
+    console.log('Failed to connect to MongoDB !.', error.message);
   }
 };
 
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const connectToMongoDBByMongoose = async () => {
   try {
     const mongodbUri = process.env.DB_MONGODB_URL;
     await mongoose.connect(mongodbUri);
-    console.log("Successfully connected to MongoDB using Mongoose.");
+    console.log('Successfully connected to MongoDB using Mongoose.');
   } catch (error) {
     console.log(
-      "Failed to connect to MongoDB using Mongoose !.",
+      'Failed to connect to MongoDB using Mongoose !.',
       error.message
     );
   }
 };
 
-import { pool } from "./db.config.js";
+import { pool } from './db.config.js';
 const connectToMySQL = async () => {
   try {
     await pool.getConnection();
-    console.log("Successfully connected to MySQL.");
+    console.log('Successfully connected to MySQL.');
     return pool;
   } catch (error) {
-    console.log("Failed to connect to MySQL", error.message);
+    console.log('Failed to connect to MySQL', error.message);
   }
 };
 // connectToMySQL();
-import { sequelize } from "./db.config.js";
+import { sequelize } from './db.config.js';
 const connectToMySQLBySequelize = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Successfully connected to MySQL using Sequelize.");
+    console.log('Successfully connected to MySQL using Sequelize.');
     return sequelize;
   } catch (error) {
-    console.log("Failed to connect to MySQL using Sequelize !.", error.message);
+    console.log('Failed to connect to MySQL using Sequelize !.', error.message);
   }
 };
 const synchronizeModels = async () => {
   try {
     // Synchronize all defined models to the database
     await sequelize.sync({}); // Use { alter: true } to avoid dropping tables
-    console.log("All models were synchronized successfully.");
+    console.log('All models were synchronized successfully.');
   } catch (error) {
-    console.log("Error synchronizing models:", error.message);
+    console.log('Error synchronizing models:', error.message);
   }
 };
 
